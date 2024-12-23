4ab55a53dd098b6feba5926a55111bc6bc5c5d3f
CyberSphinxxx <johnlemargonzales@gmail.com>
2024-12-24 03:04:03 +0800

    Add Console Practice Code

--- logs/Day33/javascript/app.js
+++ logs/Day33/javascript/app.js
@@ -1,1 +1,8 @@
-console.log(`Hello World`)
+let person = {
+    name: 'Pogi',
+    age: 20,
+};
+
+console.log(`Hello World`);
+console.log(`Hello ` + person.name );
+console.log(person.name + ` is ` + person.age + ` years old`);
