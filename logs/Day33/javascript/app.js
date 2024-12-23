3278950b135ade2c10095544b3a17905cb78d2aa
CyberSphinxxx <johnlemargonzales@gmail.com>
2024-12-24 03:10:21 +0800

    Add String Interpolation Example

--- logs/Day33/javascript/app.js
+++ logs/Day33/javascript/app.js
@@ -1,8 +1,10 @@
 let person = {
     name: 'Pogi',
     age: 20,
+    school: 'USTP',
 };
 
 console.log(`Hello World`);
 console.log(`Hello ` + person.name );
 console.log(person.name + ` is ` + person.age + ` years old`);
+console.log(`${person.name} is a ${person.school} student`);
