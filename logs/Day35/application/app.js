66f78f76cdb7356117163af471b4a24edc1f92de
CyberSphinxxx <johnlemargonzales@gmail.com>
2024-12-26 23:00:38 +0800

    Add Welcome Card

---
+++ logs/Day35/application/app.js
@@ -0,0 +1,8 @@
+
+//console.log(`Warning!`)
+//window.alert(`Virus Detected!`)
+
+//document.getElementById('myh1').textContent = `hello`;
+
+let example = document.getElementById(`welcome-msg`)
+console.log(example)
