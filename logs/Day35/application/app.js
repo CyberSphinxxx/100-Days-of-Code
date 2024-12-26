ac939f40113cd57a7d784bbf48a23178e66512ec
CyberSphinxxx <johnlemargonzales@gmail.com>
2024-12-26 23:06:53 +0800

    Add Welcome Message with Username

--- logs/Day35/application/app.js
+++ logs/Day35/application/app.js
@@ -4,5 +4,10 @@
 
 //document.getElementById('myh1').textContent = `hello`;
 
-let example = document.getElementById(`welcome-msg`)
-console.log(example)
+//let example = document.getElementById(`welcome-msg`)
+//console.log(example);
+
+
+let username = 'CyberSphinx';
+
+document.getElementById(`welcome-msg`).textContent = `Welcome ` + username;
