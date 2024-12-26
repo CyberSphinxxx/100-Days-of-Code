eddfee0ea2480fbda58970d97855bba3c1aca458
CyberSphinxxx <johnlemargonzales@gmail.com>
2024-12-26 23:46:17 +0800

    Add User-Input

--- logs/Day35/application/app.js
+++ logs/Day35/application/app.js
@@ -7,7 +7,24 @@
 //let example = document.getElementById(`welcome-msg`)
 //console.log(example);
 
+let username_input;
 
-let username = 'CyberSphinx';
+document.getElementById(`submit-btn`).onclick = function(){
+    username_input = document.getElementById(`username-input`).value;
 
-document.getElementById(`welcome-msg`).textContent = `Welcome ` + username;
+    if (username_input.trim() === "") {
+        alert(`Please enter a username!`)
+    }
+    else {
+        document.getElementById('user-greet').textContent = `Welcome ` + username_input + `, this is a sample message.`;
+        document.getElementById(`welcome-msg`).textContent = `Welcome ` + username_input;
+    }
+};
+
+document.getElementById('reset-btn').addEventListener('click', function () {
+    document.getElementById('username-input').value = '';
+
+    document.getElementById('user-greet').textContent = '';
+    document.getElementById('welcome-msg').textContent = '';
+});
+
