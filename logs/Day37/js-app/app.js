a79f0984b7a9255057f1fd8ab02e5827d07acca9
CyberSphinxxx <johnlemargonzales@gmail.com>
2024-12-28 05:49:15 +0800

    Add Practice File

---
+++ logs/Day37/js-app/app.js
@@ -0,0 +1,13 @@
+
+let username;
+
+document.getElementById('submit-btn').onclick = function(){
+    
+    username = document.getElementById('username-input').value;
+    document.getElementById('greet-username').textContent = `Welcome ` + username + "!";
+};
+
+document.getElementById('reset-btn').onclick = function(){
+
+    document.getElementById('greet-username').textContent = '';
+};
