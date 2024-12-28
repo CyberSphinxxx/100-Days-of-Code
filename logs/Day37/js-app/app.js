cfea45aae4baea7872eb81c4c270331cd9acb954
CyberSphinxxx <johnlemargonzales@gmail.com>
2024-12-28 14:52:05 +0800

    Remove Login Container if age < 18

--- logs/Day37/js-app/app.js
+++ logs/Day37/js-app/app.js
@@ -12,3 +12,15 @@ document.getElementById('reset-btn').onclick = function(){
     document.getElementById('greet-username').textContent = '';
     document.getElementById('username-input').value = '';
 };
+
+let age = window.prompt('Enter Your Age to Continue');
+
+if (age < 18) {
+    window.alert('You Cannot Enter');
+    const loginElement = document.querySelector('.login');
+    if (loginElement) {
+        loginElement.remove();
+    }
+} else {
+    window.alert('You Can Enter');
+}
