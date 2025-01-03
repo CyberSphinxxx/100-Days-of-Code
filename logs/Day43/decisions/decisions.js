51ee0f25354b0b71478bbb89dc791cb0a725f675
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-03 20:59:46 +0800

    Add Files

--- logs/Day43/decisions/decisions.js
+++ logs/Day43/decisions/decisions.js
@@ -1,5 +1,5 @@
 
-
+/*
 let age = 10;
 
 if (age < 18){
@@ -9,3 +9,25 @@ if (age < 18){
 else {
     console.log('You can enter')
 }
+*/
+
+let userAge =  document.getElementById('userAge');
+let submitBtn = document.getElementById('submitBtn');
+let resetBtn = document.getElementById('resetBtn');
+let age;
+
+submitBtn.onclick = function(){
+
+    age = Number(userAge.value);
+    if (age >= 18){
+        window.alert('You Can Enter')
+    }
+    else {
+        window.alert('You Cannot Enter')
+    }
+}
+
+resetBtn.onclick = function(){
+    userAge.value = '';
+    age = undefined;
+};
