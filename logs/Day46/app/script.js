5cef3b8508b88254ab3f0843b1804086c85ed75e
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-06 12:32:07 +0800

    Add Forumula

--- logs/Day46/app/script.js
+++ logs/Day46/app/script.js
@@ -3,6 +3,7 @@ let submitBtn = document.getElementById('submitBtn');
 let resetBtn = document.getElementById('resetBtn');
 let radiusInput = document.getElementById('radiusInput');
 let result = document.getElementById('result');
+let formula = document.getElementById('formula');
 
 const PI = Math.PI;
 let radius;
@@ -12,9 +13,11 @@ submitBtn.onclick = function() {
     radius = radiusInput.value;
     circumference = 2 * PI * radius;
     result.textContent = "Result: " + circumference;
+    formula.textContent = "Circumference = 2 x 3.14 x " + radius + " = " + circumference
 }
 
 resetBtn.onclick = function() {
     radiusInput.value = '';
     result.textContent = '';
+    formula.textContent = '';
 }
