37f8a28aec27258f56d7da7243f5760ed7b87244
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-08 20:25:10 +0800

    Add Js Variables

---
+++ logs/Day48/checked-property/script.js
@@ -0,0 +1,13 @@
+
+let subscribeBtn    = document.getElementById('subscribeBtn');
+
+let visaBtn         = document.getElementById('visaBtn');
+let mastercardBtn   = document.getElementById('mastercardBtn');
+let gcashBtn        = document.getElementById('gcashBtn');
+let paymaya         = document.getElementById('paymaya');
+
+let submitBtn       = document.getElementById('submitBtn');
+let resetBtn        = document.getElementById('resetBtn');
+
+let output          = document.getElementById('output');
+
