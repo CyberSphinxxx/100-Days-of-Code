d06efed83d620d8650c2e86801acd368b1b50250
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-11 12:39:07 +0800

    Update Js File

--- logs/Day51/app/app.js
+++ logs/Day51/app/app.js
@@ -1,6 +1,43 @@
-
+/*
 let age = 20;
 
 let message = age <= 18 ? "You Cannot Enter" : "You CAN Enter";
 
 console.log(message);
+
+*/
+
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log('Monday')
+        break;
+
+    case 2:
+        console.log('Tuesday')
+        break;
+    
+    case 3:
+        console.log('Wednesday')
+        break;
+
+    case 4:
+        console.log('Thursday')
+        break;
+
+    case 5:
+        console.log('Friday')
+        break;
+
+    case 6:
+        console.log('Saturday')
+        break;
+    
+    case 7:
+        console.log('Sunday')
+        break;
+    default:
+        console.log(`${day} is not a day`);
+        break;
+}
