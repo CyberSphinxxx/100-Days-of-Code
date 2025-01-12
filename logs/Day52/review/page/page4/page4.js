8bf43019f0be165abf17376deaa6e64a3fb75587
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-12 23:02:13 +0800

    Add Js Variable Declaration

---
+++ logs/Day52/review/page/page4/page4.js
@@ -0,0 +1,6 @@
+
+let submitBtn = document.getElementById('submitBtn');
+let resetBtn = document.getElementById('resetBtn');
+let gradeInput = document.getElementById('gradeInput');
+let gradeStat = document.getElementById('gradeStat');
+let grade;
