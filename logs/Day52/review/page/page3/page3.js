59021eea42ad32816eb578c7c79d41c3a63269ac
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-12 22:31:48 +0800

    Variable Declarations

---
+++ logs/Day52/review/page/page3/page3.js
@@ -0,0 +1,5 @@
+
+let ageInput = document.getElementById('ageInput');
+let submitBtn = document.getElementById('submitBtn');
+let resetBtn = document.getElementById('resetBtn');
+let result = document.getElementById('result');
