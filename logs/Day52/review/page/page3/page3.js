8bcf6b6fbf65d61e5dcf14190b6a4b9de936e6fe
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-12 22:36:26 +0800

    Add Main Js Function

--- logs/Day52/review/page/page3/page3.js
+++ logs/Day52/review/page/page3/page3.js
@@ -3,3 +3,20 @@ let ageInput = document.getElementById('ageInput');
 let submitBtn = document.getElementById('submitBtn');
 let resetBtn = document.getElementById('resetBtn');
 let result = document.getElementById('result');
+let age;
+
+submitBtn.onclick = function() {
+
+    age = ageInput.value;
+    if (age <= 17) {
+        window.alert('You Cannot Enter');
+    }
+    
+    else if (age >= 18){
+        window.alert('You CAN Enter');
+    }
+
+    else {
+        Window.alert('Invalid Input');
+    }
+};
