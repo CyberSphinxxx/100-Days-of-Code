7c6d0562da249b54e470d07238cf88959c65ac6f
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-12 22:07:57 +0800

    Variable Declarations

---
+++ logs/Day52/review/page/page2/page2.js
@@ -0,0 +1,6 @@
+
+let numberIndicator = document.getElementById('numberIndicator');
+let decreaseBtn = document.getElementById('decreaseBtn');
+let resetBtn = document.getElementById('resetBtn');
+let increaseBtn = document.getElementById('increaseBtn');
+
