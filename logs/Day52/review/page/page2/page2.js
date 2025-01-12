59acc4a18f22ba5104a7dee10ecf7d4162ff71d7
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-12 22:11:41 +0800

    Add Main Js Function

--- logs/Day52/review/page/page2/page2.js
+++ logs/Day52/review/page/page2/page2.js
@@ -4,3 +4,21 @@ let decreaseBtn = document.getElementById('decreaseBtn');
 let resetBtn = document.getElementById('resetBtn');
 let increaseBtn = document.getElementById('increaseBtn');
 
+let number = 0;
+
+decreaseBtn.onclick = function(){
+    number--;
+    numberIndicator.textContent = number;
+};
+
+resetBtn.onclick = function(){
+    number = 0;
+    numberIndicator.textContent = number;
+};
+
+increaseBtn.onclick = function(){
+    number++;
+    numberIndicator.textContent = number;
+};
+
+
