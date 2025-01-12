d56a9b523369bb20812fec0c5f300be3a492574e
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-13 03:40:47 +0800

    Add String Lenght Counter Function

--- logs/Day53/stringMethods/stringMethods.js
+++ logs/Day53/stringMethods/stringMethods.js
@@ -1,5 +1,13 @@
 
-let name = 'John Lemar Gonzales';
-let length = name.length;
+let submitBtn = document.getElementById('submitBtn');
+let resetBtn = document.getElementById('resetBtn');
+let textLengthInput = document.getElementById('textLengthInput');
+let stringLengthResult = document.getElementById('stringLengthResult');
+let stringInput;
+let stringLength;
 
-console.log(length);
+submitBtn.onclick = function(){
+    stringInput = textLengthInput.value;
+    stringLength = stringInput.length;
+    stringLengthResult.textContent = stringLength;
+}
