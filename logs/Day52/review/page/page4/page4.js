22da4229d3c86a5cc463fd4755ee08b2da2c04ff
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-12 23:16:17 +0800

    Add Files

--- logs/Day52/review/page/page4/page4.js
+++ logs/Day52/review/page/page4/page4.js
@@ -4,3 +4,47 @@ let resetBtn = document.getElementById('resetBtn');
 let gradeInput = document.getElementById('gradeInput');
 let gradeStat = document.getElementById('gradeStat');
 let grade;
+
+submitBtn.onclick = function() {
+
+    grade = gradeInput.value;
+    
+    switch (grade = true) {
+        case (grade <0 || grade >100):
+            gradeStat.textContent = 'Invalid Input';
+            break;
+
+        case (grade >95 && grade <100 ):
+            gradeStat.textContent = 'Excellent: A';
+            break;
+
+        case (grade >90 && grade <94.99 ):
+            gradeStat.textContent = 'Very Good: B+';
+            break;
+
+        case (grade >85 && grade <89.99 ):
+            gradeStat.textContent = 'Good: B';
+            break;
+
+        case (grade >80 && grade <84.99 ):
+            gradeStat.textContent = 'Satisfactory: C';
+            break;
+
+        case (grade >75 && grade <79.99 ):
+            gradeStat.textContent = 'Fairly Satisfactory: C';
+            break;
+
+        case (grade >=0 && grade <74.99 ):
+            gradeStat.textContent = 'Needs Special Help: F';
+            break;
+
+        default:
+            gradeStat.textContent = 'Invalid Input';
+            break;
+    }
+}
+
+resetBtn.onclick = function() {
+    gradeInput.value = '';
+    
+}
