96501047cfe9b28a25cb4f296c22c9c655e0e48d
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-18 02:06:02 +0800

    Add: Variable Declarations

---
+++ logs/Day58/strLen/strLen.js
@@ -0,0 +1,6 @@
+
+let stringInput = document.getElementById('stringInput');
+let submitBtn = document.getElementById('submitBtn');
+let resetBtn = document.getElementById('resetBtn');
+let output = document.getElementById('output');
+let submittedString;
