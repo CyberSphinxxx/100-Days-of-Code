bf94f66a8fc19c35706bdfb4331bc94d48b04585
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-18 02:10:36 +0800

    Add Main Function

--- logs/Day58/strLen/strLen.js
+++ logs/Day58/strLen/strLen.js
@@ -4,3 +4,12 @@ let submitBtn = document.getElementById('submitBtn');
 let resetBtn = document.getElementById('resetBtn');
 let output = document.getElementById('output');
 let submittedString;
+let stringLength;
+
+submitBtn.onclick = function(){
+
+    submittedString = stringInput.value;
+    stringLength = submittedString.length;
+    output.textContent = stringLength;
+
+};
