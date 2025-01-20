6f82e151c909d0d9d4da21b86d0867e8485fc699
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-20 23:24:24 +0800

    Add Files

---
+++ logs/Day60/script.js
@@ -0,0 +1,22 @@
+
+
+/*
+usesrname = username.trim();
+
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+
+let extraChar = username.slice(1);
+extraChar = extraChar.toLowerCase();
+
+username = letter + extraChar
+
+console.log(username);
+
+let username = window.prompt('Enter Username');
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+
+console.log(username);
+*/
+
