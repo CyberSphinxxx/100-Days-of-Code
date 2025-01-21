bfbdd4724c01193379e350fd58fbbbf1429e3c28
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-21 14:39:42 +0800

    Add Files

---
+++ logs/Day61/loginjs/login.js
@@ -0,0 +1,6 @@
+
+let firstnameInput = document.getElementById('firstnameInput');
+let emailInput = document.getElementById('emailInput');
+let passwordInput = document.getElementById('passwordInput');
+let repeatPasswordInput = document.getElementById('repeatPasswordInput');
+let signupBtn = document.getElementById('signupBtn');
