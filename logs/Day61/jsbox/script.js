5622e3f6a6f5fc5f2716cf0438c6aaabac886d2e
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-01-21 13:26:50 +0800

    Add Files

--- logs/Day61/jsbox/script.js
+++ logs/Day61/jsbox/script.js
@@ -29,3 +29,23 @@ else {
 }
 
 */
+
+let usernameInput = document.getElementById('usernameInput');
+let submitBtn = document.getElementById('submitBtn');
+let resetBtn = document.getElementById('resetBtn');
+let result = document.getElementById('result');
+let username;
+
+submitBtn.onclick = function(){
+
+    while (usernameInput === '' || username === null) {
+        window.alert('Please Enter a Username!')
+    }
+    username = usernameInput.value;
+    result.textContent = username;
+}
+
+resetBtn.onclick = function(){
+    usernameInput.value = '';
+    result.textContent = '';
+}
